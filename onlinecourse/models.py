3d58c3808b9f774b2cf38a7782f489adc8b13c30
elios11 <eliosferre3@hotmail.com>
2023-03-07 20:22:48 -0500

    Update repository for Lab 3

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -136,7 +136,8 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     is_correct = models.BooleanField(default=False)
-
+    def __str__(self):
+        return f"Choice for question: {self.question.id}"
 
 # <HINT> The submission model
 # One enrollment could have multiple submission
